ea2e21e766a277da80e8f1d8702c88af115bb2e6
Andrew Cockerham <cockerham.andrew@gmail.com>
2014-01-20 05:55:22 -0600

    teacher email to permit params

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -73,6 +73,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:title, :description, :teacher, :end_date)
+      params.require(:course).permit(:title, :description, :teacher, :end_date, :teacher_email)
     end
 end
