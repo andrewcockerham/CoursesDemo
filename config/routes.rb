4644ff64fb1d7d2bdde97f065049edd51f06beb4
Andrew Cockerham <cockerham.andrew@gmail.com>
2014-01-30 20:43:22 -0600

    adf

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 CoursesDemo::Application.routes.draw do
-  devise_for :students
   
+
   root "pages#home"
 
   resources :registrations
 
   resources :students
-
+  devise_for :students
+  
   resources :courses
 
   match 'courses/register/:id', to: 'registrations#new', as: :register, via: [:get, :post]
