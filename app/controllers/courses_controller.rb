7b524e2820a2128615fcdb741603d48d63b08617
Andrew Cockerham <cockerham.andrew@gmail.com>
2014-02-01 15:08:28 -0600

    add complete

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -79,6 +79,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:title, :description, :teacher, :end_date, :teacher_email)
+      params.require(:course).permit(:title, :description, :teacher, :end_date, :teacher_email, :complete)
     end
 end
