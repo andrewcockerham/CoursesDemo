64e5905cf8f3d857434e02b4d7191f7afe3c9fd3
Andrew Cockerham <cockerham.andrew@gmail.com>
2014-02-02 10:16:10 -0600

    add paperclip content_type validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,4 +6,7 @@ class Course < ActiveRecord::Base
 	has_many :users, through: :attendances
 
 	has_attached_file :pdf
+
+	validates_attachment_content_type :pdf, content_type: {'application/pdf'},
+																		size: { less_than 10.megabytes }
 end
