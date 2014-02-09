a25646e103e87a8ca95580c91d2b54fa4dab1445
Andrew Cockerham <cockerham.andrew@gmail.com>
2014-02-09 15:02:20 -0600

    fix show pdf if ni

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, except: [:index]
 
   # my custom actions
   def my_courses
